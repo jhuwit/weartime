5379eb02aec83afd67e8252669852a7f149090ca
muschellij2 <muschellij2@gmail.com>
2024-04-17 13:44:05 -0400

    functions now work

--- inst/extdata/pull_new_functions.sh
+++ inst/extdata/pull_new_functions.sh
@@ -1,3 +1,4 @@
+cd inst/extdata
 rm -rf CNN-Non-Wear-Time-Algorithm
 git clone --depth=1 https://github.com/muschellij2/CNN-Non-Wear-Time-Algorithm CNN-Non-Wear-Time-Algorithm
 rm -rf cnn
@@ -6,3 +7,4 @@ mkdir -p cnn/functions/
 cp CNN-Non-Wear-Time-Algorithm/functions/*.py cnn/functions
 rm -f cnn/functions/gt3x_functions.py
 rm -rf CNN-Non-Wear-Time-Algorithm
+cd ../../
